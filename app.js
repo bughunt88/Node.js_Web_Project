3781c2a6851daa84363fbb32dbcaaaff20f4bb4c
hansoo <hansoo@hansooui-MacBook-Pro.local>
2017-01-25 13:32:16 +0900

    good

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ var flash          = require('connect-flash'); //session에 한번 저장해서 
 var mongo          = require('mongodb');
 var Grid           = require('gridfs-stream');
 var fs             = require("fs");
-var multer         = require("multer");
-var _storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-   cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-var upload = multer({ storage: _storage });
+// var multer         = require("multer");
+// var _storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//    cb(null, 'uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   }
+// });
+// var upload = multer({ storage: _storage });
 
 app.use('/user',express.static('uploads'));
 
